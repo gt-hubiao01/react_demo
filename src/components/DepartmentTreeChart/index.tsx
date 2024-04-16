87fe450d9faf858d14c69d590e0d6654dbac5320
hubiao01 <hubiao01@baijia.com>
2024-04-16 16:19:43 +0800

    feat: 设置Tree初始在容器正中间

--- src/components/DepartmentTreeChart/index.tsx
+++ src/components/DepartmentTreeChart/index.tsx
@@ -1,46 +1,62 @@
+import { useEffect, useState, useRef } from 'react'
 import styles from './index.module.less'
 import Tree from 'react-d3-tree'
 
-function DepartmentTreeChart() {
-  const orgChart = {
-    name: 'CEO',
-    children: [
-      {
-        name: 'Manager',
-        attributes: {
-          department: 'Production',
-        },
-        children: [
-          {
-            name: 'Foreman',
-            attributes: {
-              department: 'Fabrication',
-            },
-            children: [
-              {
-                name: 'Worker',
-              },
-            ],
-          },
-          {
-            name: 'Foreman',
-            attributes: {
-              department: 'Assembly',
-            },
-            children: [
-              {
-                name: 'Worker',
-              },
-            ],
-          },
-        ],
+const orgChart = {
+  name: 'CEO',
+  children: [
+    {
+      name: 'Manager',
+      attributes: {
+        department: 'Production',
       },
-    ],
-  }
+      children: [
+        {
+          name: 'Foreman',
+          attributes: {
+            department: 'Fabrication',
+          },
+          children: [
+            {
+              name: 'Worker',
+            },
+          ],
+        },
+        {
+          name: 'Foreman',
+          attributes: {
+            department: 'Assembly',
+          },
+          children: [
+            {
+              name: 'Worker',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
+
+function DepartmentTreeChart() {
+  const [translate, setTransLate] = useState({ x: 0, y: 0 })
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const { width } = containerRef.current.getBoundingClientRect()
+      setTransLate({ x: width / 2, y: 50 })
+    }
+  }, [])
 
   return (
-    <div className={styles.treeContainer}>
-      <Tree data={orgChart} initialDepth={1} />
+    <div className={styles.treeContainer} ref={containerRef}>
+      <Tree
+        data={orgChart}
+        initialDepth={3}
+        orientation="vertical"
+        translate={translate}
+      />
     </div>
   )
 }
